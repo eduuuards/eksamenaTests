dde432ec6fbe52ddc1d340f2905a7ea3e51f3083
eduuuards <eduards.polis2@gmail.com>
2022-06-10 17:31:30 +0300

    Pievienoti vel vairak jautajumi, kopa 8 pagaidam

--- src/eksamenaTests/Tests.java
+++ src/eksamenaTests/Tests.java
@@ -23,13 +23,25 @@ public class Tests {
 	String j5 = "Cik argumentus vajag funkcijai pow() ?\n"
 			+"[a] 2 [b] vairak par 1 bet mazak par 3 [c] 1 [d] 4 \n";
 	
+	String j6 = "Kuras funkcijas sanemot divas vertibas, dara vienu un to pasu tikai otradi? ?\n"
+			+"[a] pow() [b] min() [c] sqrt() [d] max() \n";
+	
+	String j7 = "Kuras funkcijas ir vienadas?\n"
+			+"[a] acos() [b] cos() [c] Math.cos() [d] cosine() \n";
+	
+	String j8 = "Kuras funkcijas atgriez double vertibu??\n"
+			+"[a] floor() [b] ceil() [c] round() [d] rint() \n";
+	
 	
 	Jautajums [] jautajumi = {
 			new Jautajums(j1, "ab"),
 			new Jautajums(j2, "ac"),
 			new Jautajums(j3, "abc"),
 			new Jautajums(j4, "ac"),
-			new Jautajums(j5, "ab")
+			new Jautajums(j5, "ab"),
+			new Jautajums(j6, "bd"),
+			new Jautajums(j7, "bc"),
+			new Jautajums(j8, "abd")
 	};
 	testaIzpilde(jautajumi);
 	
@@ -45,9 +57,6 @@ public class Tests {
 			atbilde = read.nextLine();
 			}while(!atbilde.equals(jautajumi[i].atbilde));
 				
-			
-		
-		
 		}
 		System.out.println("Tests ir pabeigts.");
 		read.close();
