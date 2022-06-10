8bae1d30a1cc4894bf513a1b8172af6f18f3ed2e
eduuuards <eduards.polis2@gmail.com>
2022-06-10 16:42:18 +0300

    Iesakta jautajumu izveide

---
+++ src/eksamenaTests/Jautajums.java
@@ -0,0 +1,11 @@
+package eksamenaTests;
+
+public class Jautajums {
+	String jaut;
+	String atbilde;
+	
+	public Jautajums(String jaut, String atbilde) {
+	this.jaut = jaut;
+	this.atbilde = atbilde;
+}
+}
--- src/eksamenaTests/Tests.java
+++ src/eksamenaTests/Tests.java
@@ -4,8 +4,26 @@ public class Tests {
 
 	public static void main(String[] args) {
 	System.out.println("Tests par skaitliskam funkcijam");
-	System.out.println("Pareizas atbildes ievadi kopa bez atstarpēm alfabeta seciba.");
+	System.out.println("Pareizas atbildes ievadi kopa bez atstarpem alfabeta seciba.");
 
+	String j1 = "Kuri datu tipi strādā ar cipariem?\n"
+			+"[a] int  [b] double [c] String";
+	String j2 = "Kuras  atbildes raksturo datu tipu 'double'?\n"
+			+"[a] Cipari  [b] Burti [c] Decimaldalas [d] Cikls";
+	
+	
+	
+	
+	Jautajums [] jautajumi = {
+			new Jautajums(j1, "ab"),
+			new Jautajums(j2, "ac")
+	}
+	}
+	
+	
+	public static void testaIzpilde(Jautajums [] jautajumi) {
+		
+		
 	}
 
 }
