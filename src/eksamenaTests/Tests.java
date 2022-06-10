51992d8fffe39bb9384b4d77d7fb710b370d605c
eduuuards <eduards.polis2@gmail.com>
2022-06-10 16:54:40 +0300

    Pabeigts pirmais jautajums, atbilzu pienemsana strada

--- src/eksamenaTests/Tests.java
+++ src/eksamenaTests/Tests.java
@@ -1,13 +1,15 @@
 package eksamenaTests;
 
+import java.util.Scanner;
+
 public class Tests {
 
 	public static void main(String[] args) {
 	System.out.println("Tests par skaitliskam funkcijam");
 	System.out.println("Pareizas atbildes ievadi kopa bez atstarpem alfabeta seciba.");
 
-	String j1 = "Kuri datu tipi strādā ar cipariem?\n"
-			+"[a] int  [b] double [c] String";
+	String j1 = "\nKuri datu tipi strada ar cipariem?\n"
+			+"[a] int  [b] double [c] String\n";
 	String j2 = "Kuras  atbildes raksturo datu tipu 'double'?\n"
 			+"[a] Cipari  [b] Burti [c] Decimaldalas [d] Cikls";
 	
@@ -17,13 +19,29 @@ public class Tests {
 	Jautajums [] jautajumi = {
 			new Jautajums(j1, "ab"),
 			new Jautajums(j2, "ac")
-	}
-	}
 	
+	};
+	testaIzpilde(jautajumi);
+	
+	}
 	
 	public static void testaIzpilde(Jautajums [] jautajumi) {
+		Scanner read = new Scanner(System.in);
+		
+		for(int i=0; i<jautajumi.length; i++) {
+			String atbilde;
+			do {
+			System.out.println(jautajumi[i].jaut);
+			atbilde = read.nextLine();
+			}while(!atbilde.equals(jautajumi[i].atbilde));
+				
+			
 		
 		
+		}
+		System.out.println("Tests ir pabeigts.");
+		read.close();
 	}
 
 }
+
