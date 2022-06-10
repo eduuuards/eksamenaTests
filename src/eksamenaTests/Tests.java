61b8fe15b4b96f01510bb6e38ca2be0910542c15
eduuuards <eduards.polis2@gmail.com>
2022-06-10 17:10:59 +0300

    Papildinats tests ar vairak jautajumiem

--- src/eksamenaTests/Tests.java
+++ src/eksamenaTests/Tests.java
@@ -9,17 +9,27 @@ public class Tests {
 	System.out.println("Pareizas atbildes ievadi kopa bez atstarpem alfabeta seciba.");
 
 	String j1 = "\nKuri datu tipi strada ar cipariem?\n"
-			+"[a] int  [b] double [c] String\n";
+			+"[a] int  [b] double [c] String [d]SQL\n ";
+	
 	String j2 = "Kuras  atbildes raksturo datu tipu 'double'?\n"
-			+"[a] Cipari  [b] Burti [c] Decimaldalas [d] Cikls";
+			+"[a] Cipari  [b] Burti [c] Decimaldalas [d] Cikls\n";
 	
+	String j3 = "Kuras  atbildes raksturo datu tipu 'String'?\n"
+			+"[a] Teksts  [b] Burti [c] Cipari [d] For Cikls\n";
 	
+	String j4 = "Kuras funkcijas pieder trigonometrijai?\n"
+			+"[a] sin()  [b] pow() [c] tan() [d] abs()\n";
+	
+	String j5 = "Cik argumentus vajag funkcijai pow() ?\n"
+			+"[a] 2 [b] vairak par 1 bet mazak par 3 [c] 1 [d] 4 \n";
 	
 	
 	Jautajums [] jautajumi = {
 			new Jautajums(j1, "ab"),
-			new Jautajums(j2, "ac")
-	
+			new Jautajums(j2, "ac"),
+			new Jautajums(j3, "abc"),
+			new Jautajums(j4, "ac"),
+			new Jautajums(j5, "ab")
 	};
 	testaIzpilde(jautajumi);
 	
